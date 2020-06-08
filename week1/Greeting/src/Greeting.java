2b9a0c394986a2e516db056a3647876bfe4f909d
vuhaile <levuhai207@hotmail.com.vn>
2020-06-08 23:14:35 +0700

    hoan thanh bai tap hien thi loi chao

---
+++ week1/Greeting/src/Greeting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String name;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        name = scanner.nextLine();
+        System.out.printf("Hello %S",name);
+    }
+}
