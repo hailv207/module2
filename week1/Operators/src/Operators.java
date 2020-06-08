9982cc8736584eb9aa1fcade49d3da1bb77e91bb
vuhaile <levuhai207@hotmail.com.vn>
2020-06-08 21:31:50 +0700

    hoan thanh bai thuc hanh toan tu

---
+++ week1/Operators/src/Operators.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao chieu cao:");
+        height = scanner.nextFloat();
+        System.out.println("Nhap vao chieu rong:");
+        width = scanner.nextFloat();
+        System.out.println("Dien tich hinh chu nhat la: " + width * height);
+    }
+}
