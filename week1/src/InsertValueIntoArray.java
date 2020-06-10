4ac402fc6ada22be3665cb808b896c39c680bcac
vuhaile <levuhai207@hotmail.com.vn>
2020-06-10 11:35:21 +0700

    hoan thanh bai tap them phan tu vao mang

---
+++ week1/src/InsertValueIntoArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class InsertValueIntoArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("The original Array:");
+        DeleteValueFromArray.displayIntArray(array);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the value:");
+        int value = scanner.nextInt();
+        System.out.println("Please enter the index your want to insert value:");
+        ;
+        int index = scanner.nextInt();
+        insertValueIntoArray(value, index, array);
+        System.out.println("The new Array:");
+        DeleteValueFromArray.displayIntArray(array);
+    }
+
+    public static void insertValueIntoArray(int value, int index, int[] array) {
+        for (int i = array.length - 1; i >= index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+    }
+}
